cee4419c388ed225cc92db5afbc57da1d469684e
sandeepaksm <sandy2411996@gmail.com>
2021-08-01 22:25:26 +0530

    sandeep testing

--- sandeep_testing.py
+++ sandeep_testing.py
@@ -1,3 +1,19 @@
+f_no = 1
+s_no = 2
+print(f_no)
+print(s_no)
+result = 2
+while True:
+    t_no = f_no + s_no
+    if t_no >= 4000000:
+        break
+    if t_no % 2 == 0:
+        result += t_no # result = result + t_no
+    print(t_no)
+    f_no = s_no
+    s_no = t_no
+print()
+print("Sum of even numbers till 4000000 is ", result)
 index=(12,22,55,66,88,98,4624,33)
 count_even =0
 count_odd =0
@@ -24,23 +40,3 @@ while index<20:
         print("none of intergers")
 print("total number even number",count_even)
 print("total number odd number",count_odd)
-print("total number prime number",count_prime)
-
-
-
-f_no = 1
-s_no = 2
-print(f_no)
-print(s_no)
-result = 2
-while True:
-    t_no = f_no + s_no
-    if t_no >= 4000000:
-        break
-    if t_no % 2 == 0:
-        result += t_no # result = result + t_no
-    print(t_no)
-    f_no = s_no
-    s_no = t_no
-print()
-print("Sum of even numbers till 4000000 is ", result)
